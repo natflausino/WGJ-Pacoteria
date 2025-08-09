b3fe2cb0ab4063f6f36b548487fe359a772b74af
natflausino <natasha.flausino@hotmail.com>
2025-08-09 18:38:34 -0300

    🚧:construction: add ajusts + credits menu and funcionalities

---
+++ Pacoteria/main_menu/change_scenes_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu/credits.tscn")
+
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu/settings.tscn")
+	
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://main_menu/menu.tscn")
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
