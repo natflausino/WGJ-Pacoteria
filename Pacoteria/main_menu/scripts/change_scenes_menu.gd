50ddd83c39d55fd08e4d854af83aecc2a8cdc40c
natflausino <natasha.flausino@hotmail.com>
2025-08-13 18:04:03 -0300

    ✨:feat:add day3 and final game

--- Pacoteria/main_menu/scripts/change_scenes_menu.gd
+++ Pacoteria/main_menu/scripts/change_scenes_menu.gd
@@ -1,7 +1,16 @@
 extends Control
 
+func _on_endday1_pressed():
+	get_tree().change_scene_to_file("res://dias/scenes/day2/dia2_1.tscn")
+
+func _on_endday2_pressed():
+	get_tree().change_scene_to_file("res://dias/scenes/day3/dia3_1.tscn")
+
+func _on_endgame_pressed():
+	get_tree().change_scene_to_file("res://final/scene/final_jogo.tscn")
+
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://dias/scenes/dia1_1.tscn")
+	get_tree().change_scene_to_file("res://dias/scenes/day1/dia1_1.tscn")
 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://main_menu/scenes/credits.tscn")
