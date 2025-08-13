7f290e9debe03c35818b8a05b3d503fd961a7cbb
Julia Pires <jujup1209@gmail.com>
2025-08-13 17:45:16 -0300

    :recycle: refactor
    redesenhando fase 3, colocando assets e referenciando sprites

--- Pacoteria/puzzle/Fase 3/fase_3.gd
+++ Pacoteria/puzzle/Fase 3/fase_3.gd
@@ -1,20 +1,10 @@
 extends Node2D
 
 #Validacão da Celula (VCell)
-var VCell = [false, false, false, true, true, true, false, false, true, false, false]
+var VCell = [true, true, false, false, false, true, false, false]
 
 #Rotação da Celula (RCell)
-@export var RCell1: Sprite2D
-@export var RCell2: Sprite2D
-@export var RCell3: Sprite2D
-@export var RCell4: Sprite2D
-@export var RCell5: Sprite2D
-@export var RCell7: Sprite2D
-@export var RCell8: Sprite2D
-@export var RCell9: Sprite2D
-@export var RCell10: Sprite2D
-@export var RCell11: Sprite2D
-@export var RCell12: Sprite2D
+@export var RCell: Array[Sprite2D]
 
 # Busca por nós do tipo 'Button' em toda a hierarquia de descendentes.
 func _ready():
@@ -26,7 +16,7 @@ func _ready():
 #Inspecionar a rotação da celula e definir seu valor booleano
 func _on_any_button_pressed():
 		
-	if RCell1.rotation_degrees == 90:
+	if RCell[0].rotation_degrees == 0:
 		VCell[0] = true
 		print("cell1 é true")
 		determina_Vitoria()
@@ -34,7 +24,7 @@ func _on_any_button_pressed():
 		VCell[0] = false
 		print("cell1 é false")
 	
-	if RCell2.rotation_degrees == 90:
+	if RCell[1].rotation_degrees == 0:
 		VCell[1] = true
 		print("cell1 é true")
 		determina_Vitoria()
@@ -42,7 +32,7 @@ func _on_any_button_pressed():
 		VCell[1] = false
 		print("cell1 é false")
 		
-	if RCell3.rotation_degrees == 0:
+	if RCell[2].rotation_degrees == 90:
 		VCell[2] = true
 		print("cell3 é true")
 		determina_Vitoria()
@@ -50,7 +40,7 @@ func _on_any_button_pressed():
 		VCell[2] = false
 		print("cell3 é false")
 	
-	if RCell4.rotation_degrees == 0:
+	if RCell[3].rotation_degrees == 0:
 		VCell[3] = true
 		print("cell4 é true")
 		determina_Vitoria()
@@ -58,7 +48,7 @@ func _on_any_button_pressed():
 		VCell[3] = false
 		print("cell4 é false")
 		
-	if RCell5.rotation_degrees == 0:
+	if RCell[4].rotation_degrees == 90:
 		VCell[4] = true
 		print("cell5 é true")
 		determina_Vitoria()
@@ -66,7 +56,7 @@ func _on_any_button_pressed():
 		VCell[4] = false
 		print("cell5 é false")
 		
-	if RCell7.rotation_degrees == 90:
+	if RCell[5].rotation_degrees == 0:
 		VCell[5] = true
 		print("cell7 é true")
 		determina_Vitoria()
@@ -74,7 +64,7 @@ func _on_any_button_pressed():
 		VCell[5] = false
 		print("cell7 é false")
 	
-	if RCell8.rotation_degrees == 0:
+	if RCell[6].rotation_degrees == 0:
 		VCell[6] = true
 		print("cell8 é true")
 		determina_Vitoria()
@@ -82,37 +72,13 @@ func _on_any_button_pressed():
 		VCell[6] = false
 		print("cell8 é false")
 		
-	if RCell9.rotation_degrees == 90:
+	if RCell[7].rotation_degrees == 90:
 		VCell[7] = true
 		print("cell9 é true")
 		determina_Vitoria()
 	else:
 		VCell[7] = false
 		print("cell9 é false")
-		
-	if RCell10.rotation_degrees == 90:
-		VCell[8] = true
-		print("cell10 é true")
-		determina_Vitoria()
-	else:
-		VCell[8] = false
-		print("cell10 é false")
-		
-	if RCell11.rotation_degrees == 0:
-		VCell[9] = true
-		print("cell7 é true")
-		determina_Vitoria()
-	else:
-		VCell[9] = false
-		print("cell7 é false")
-	
-	if RCell12.rotation_degrees == 90:
-		VCell[10] = true
-		print("cell7 é true")
-		determina_Vitoria()
-	else:
-		VCell[10] = false
-		print("cell7 é false")
 		
 func recarregar_cena():
 	self.visible = false
