095af45c3a8cd8820798270ea1475d1977eab95a
Julia Pires <jujup1209@gmail.com>
2025-08-16 10:27:04 -0300

    ♻️ refactor
    determinando vitória por meio da classe que determina vitória

--- Pacoteria/puzzle/Fase 3/fase_3.gd
+++ Pacoteria/puzzle/Fase 3/fase_3.gd
@@ -9,18 +9,27 @@ var ERotation = [90, 0, 90, 90, 90, 0, 90, 90]
 #Rotação da Celula (RCell)
 @export var RCell: Array [Sprite2D]
 
-# Busca por nós do tipo 'Button' em toda a hierarquia de descendentes.
+#instanciando a classe que determina vitoria
+const Classevitoria = preload("res://puzzle/DVitory_class.gd")
+var Dvitory
+
 func _ready():
+	#conectando valores da classe atual com a classe que determina vitoria
+	Dvitory = Classevitoria.new()
+	add_child(Dvitory)
+	Dvitory.vitoria.connect(recarregar_cena)
+	
+	# Busca por nós do tipo 'Button' em toda a hierarquia de descendentes.
 	var buttons = find_children("", "Button", true)
+	for button in buttons:
+		button.pressed.connect(_on_any_button_pressed)
 	
 	#muda o tamanho do array de VCell para o mesmo tamanho do array de RCell
 	VCell.resize(RCell.size())
-	
-	for button in buttons:
-		button.pressed.connect(_on_any_button_pressed)
 
 #Inspecionar a rotação da celula e definir seu valor booleano
 func _on_any_button_pressed():
+		Dvitory.VCell = VCell
 		for i in range(RCell.size()):
 			if i <= ERotation.size():
 				if RCell[i].rotation_degrees == ERotation[i]:
@@ -29,20 +38,14 @@ func _on_any_button_pressed():
 				else:
 					VCell[i]=false
 					print("Cell",i+1,"é false")
-			determina_Vitoria()
+		Dvitory.determina_Vitoria()
 	
 func recarregar_cena():
+	var Victory_sound = $Victorysound as AudioStreamPlayer
+	Victory_sound.play()
+	await get_tree().create_timer(1.0).timeout
+	
 	await get_tree().process_frame
 	var result = get_tree().change_scene_to_file("res://dias/scenes/day2/dia22.tscn")
 	if result != OK:
 		print("Falha")
-		
-	#get_tree().reload_current_scene()
-# Não precisamos continuar, já sabemos que uma é false
-
-func determina_Vitoria():
-	var verifica_Valor = VCell.all(func(valor):return valor)
-	if verifica_Valor:
-		print("Todas as 7 variáveis no array são true! O jogo reiniciou.")
-		recarregar_cena()
-	
